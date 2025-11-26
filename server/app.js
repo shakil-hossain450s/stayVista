c01bb1793a81af08d007413b2ccab71c9b24e1dc
shakil <shakil.hossain450s@gmail.com>
2025-11-26 18:35:58 +0600

    cors related issue solved

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const statisticsRoutes = require('./routes/statistics.routes');
 
 // middleware
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'https://stay-vistaa.web.app'],
   credentials: true,
   optionSuccessStatus: 200,
 }
